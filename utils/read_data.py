def0fbde9de58e454bfdf5d1371e23517e689abc
Matthew Spofford <matthewspofford@verizon.net>
2022-02-15 18:46:02 -0500

    Updated read_data to use ISO-8859-1 for opening files, as well as ignoring an additional line in the data file

--- utils/read_data.py
+++ utils/read_data.py
@@ -1,17 +1,10 @@
 import pandas as pd
-import os
 
-class DataReader():
 
-    def __init__(self):
-        return None
-
-    def read(self, file_name):
-        current_dir = "samples/"
-        file_path = os.path.join(current_dir, file_name)
-        with open(file_path) as f:
-            lines = f.readlines()
-        lines = lines[4:]
-        rows = [line.split("\t")[:-1] for line in lines]
-        df = pd.DataFrame(rows, columns=["scale of analysis", "relative area", "fractal complexity", "r2"])
-        return df
+def read_data(file_name):
+    with open(file_name, encoding='ISO-8859-1') as f:
+        lines = f.readlines()
+    lines = lines[5:]
+    rows = [line.split("\t")[:-1] for line in lines]
+    df = pd.DataFrame(rows, columns=["scale of analysis", "relative area", "fractal complexity", "r2"])
+    return df
