bc832decc91affda6f2d7a041c61d6ea41302166
Dillon McCarthy <dillonmccarthy423@gmail.com>
2022-02-23 10:41:43 -0500

    very accurate interpolation function

--- multiscale_modules/utils/interpolate.py
+++ multiscale_modules/utils/interpolate.py
@@ -4,58 +4,67 @@ import numpy as np
 from read_data import *
 import sys
 import matplotlib.pyplot as plt
+import os
 
-DEGREE = 2      # Degree of polynomial regression for interpolation model
+DEGREE = 10      # Degree of polynomial regression for interpolation model
 BASE = 0        # The surface to base the scales off of
+PERIOD = 10
+HEIGHT = 2
+SHIFT = 1
 
 # Take in argument data which should be an array of all surfaces and their data
 # Function will interpolate the data based on the first surface, filling any of these
 # scales not in another surface with its predicted value based on a polynomial model
-def interpolate(data):
-    num_surfaces = len(data)
-    models = []
-    inter_data = []
-
+def interpolate(data, stat):
+    # Get the scales to be the base of inteprolation
     base = data[BASE]
-    base_x = base["scale of analysis"]
-    print("BASE SCALES= " + str(base_x))
+    base_x = base[stat]
 
+    # Use a logarithmic transformation and polynomial regression for each surface to obtain a model
+    # for the given statistic for each scale
     for surf in data:
-        # fit polynomial regression
-        # append to list of models
-        inter_data.append([])
-        yData = surf["relative area"]
-        xData = surf["scale of analysis"]
+        # Get data
+        yData = surf[stat]
+        xData = np.log(surf["scale of analysis"])
 
-        # poly = PolynomialFeatures(degree=DEGREE)
-        # features = poly.fit_transform(xData.values.reshape(-1,1))
-        # model = LinearRegression()
-        # model.fit(features,yData)
-        # models.append(model)
-        model = np.polyfit(np.log(xData), yData, 1)
-        c0 = model[1]
-        c1 = model[0]
-        l = np.log(xData)
-        y_predict = c0 + c1 * l
+        # Train model
+        weights = np.polyfit(xData, yData, DEGREE)
+        model = np.poly1d(weights)
+        xp = np.linspace(xData.min(), xData.max(), 70)
+        y_predict = model(xp)
 
-        # y_predict = model.predict(features)
-        plt.scatter(xData, yData)
-        plt.plot(xData, y_predict, c="b")
+        # Visualize data and compare with model
+        plt.scatter(xData, yData, c='b', facecolor='none', alpha=.5)
+        plt.plot(xp, y_predict, c='purple')
+        plt.xlabel("Log of Scale of Analysis")
+        plt.ylabel(stat)
         plt.show()
 
+        # Replace old data with new data aligned on the same scale
         for i, val in enumerate(xData):
             if val not in base_x:
-                yData[i] = c0 + c1 * np.log(val)
+                yData[i] = model(val)
+
+    # Visualize new plots on the same scales
     print("\n\nNew Scales:\n")
-    for surf in data:
-        print(surf["scale of analysis"])
+    colors = ['r', 'b', 'g', 'y']
+    for i, surf in enumerate(data):
+        plt.scatter(np.log(surf["scale of analysis"]), surf[stat], c=colors[i], alpha=.3)
+    plt.xlabel("Log of Scale of Analysis")
+    plt.ylabel(stat)
+    plt.show()
         
 if __name__ == "__main__":
     args = sys.argv[1:]
-    filename1 = args[0]
-    filename2 = args[1]
+    stat = args[0]
+    filename1 = args[1]
+    filename2 = args[2]
+    filename3 = args[3]
+    filename4 = args[4]
 
     data = []
     data.append(read_data(filename1))
     data.append(read_data(filename2))
-    interpolate(data)
+    data.append(read_data(filename3))
+    data.append(read_data(filename4))
+    interpolate(data, stat)
